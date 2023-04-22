0e8311499151541880a893bbbbaa513d93b4d2c9
No-ha63 <tup41475@temple.edu>
2023-04-21 21:21:58 -0400

    Changes to be committed: 	modified:   Earthquake.py
    Changes not staged for commit:
    	deleted:    Cali.jpeg
    
    Untracked files:
    	Cali.png
    	Caribbean.png
    	SouthEastAsia.png

--- Earthquake.py
+++ Earthquake.py
@@ -4,4 +4,13 @@ import tkinter
 
 
 def CaliSet():
+    turtle.setup(1246,1462)
     
+    wn = turtle.Screen()
+    wn.title('Cali Baby')
+    wn.setworldcoordinates(32.09969,-124.76537,42.18684,-114.00260)
+
+    canvas = wn.getcanvas()
+    map_bg_img = tkinter.PhotoImage(file="images/atlantic-basin.png")
+
+
