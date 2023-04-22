c7c24686b89c5ca493d06a74fb829d81a6ea485c
No-ha63 <tup41475@temple.edu>
2023-04-22 00:28:15 -0400

    new file:   Cali.png 	new file:   Caribbean.png 	modified:   Earthquake.py 	new file:   Earthquakes.csv

--- Earthquake.py
+++ Earthquake.py
@@ -4,7 +4,7 @@ import tkinter
 
 
 def CaliSet():
-    turtle.setup(1246,1462)
+    turtle.setup(873,1024)
     
     wn = turtle.Screen()
     wn.title('Cali Baby')
@@ -14,21 +14,21 @@ def CaliSet():
     canvas = wn.getcanvas()
     map_bg_img = tkinter.PhotoImage(file="PythonFinal/Cali.png")
 
-    canvas.create_image(-1356,-1452, anchor = tkinter.NW, image=map_bg_img)
+    canvas.create_image(-1000,-1000, anchor = tkinter.NW, image=map_bg_img)
 
     bob = turtle.Turtle()
 
     return bob,wn,map_bg_img
 
 def test():
-    bob,wn,map_bg_img = SEAsiaSet()
-    bob.goto(-10.97059,94.94849,)
-    bob.goto(14.96693,149.53941)
+    bob,wn,map_bg_img = CaliSet()
+    bob.goto(32.09969,-124.76537)
+    bob.goto(42.18684,-114.00260)
     turtle.exitonclick()
 
 
 def CarribeanSet():
-    turtle.setup(2492,1238)
+    turtle.setup(1024,509)
     
     wn = turtle.Screen()
     wn.title('Caribean')
@@ -38,7 +38,7 @@ def CarribeanSet():
     canvas = wn.getcanvas()
     map_bg_img = tkinter.PhotoImage(file="PythonFinal/Caribbean.png")
 
-    canvas.create_image(-1356,-1452, anchor = tkinter.NW, image=map_bg_img)
+    canvas.create_image(-500,-1024, anchor = tkinter.NW, image=map_bg_img)
 
     bob = turtle.Turtle()
 
@@ -56,14 +56,22 @@ def SEAsiaSet(): #good
     canvas = wn.getcanvas()
     map_bg_img = tkinter.PhotoImage(file="PythonFinal/SouthEastAsia.png")
 
-    canvas.create_image(-441,-1300, anchor = tkinter.NW, image=map_bg_img)
+    canvas.create_image(-438,-1300, anchor = tkinter.NW, image=map_bg_img)
 
     bob = turtle.Turtle()
 
     return bob,wn,map_bg_img  
 
 
+def distance(swaves,pwaves):
+    s_list = swaves.split(':')
+    p_list = pwaves.split(':')
+    s_sec = int(s_list[0])*60 + int(s_list[1])
+    p_sec = int(p_list[0])*60 + int(p_list[1])
+    return (p_sec-s_sec)*8.4
 
+def distanceFormap(km):
+    return km/110.574
 
 test()
 
