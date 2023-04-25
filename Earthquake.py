2f959edba54a272c18493755bcf74a5b9e16ccb9
No-ha63 <tup41475@temple.edu>
2023-04-25 14:11:12 -0400

    modified:   Earthquake.py

--- Earthquake.py
+++ Earthquake.py
@@ -2,13 +2,14 @@ import turtle
 import pandas as pd
 import tkinter
 
+#I used the irma.py to model the setup for this
 
 def CaliSet():
     turtle.setup(873,1024)
     
     wn = turtle.Screen()
     wn.title('Cali Baby')
-    wn.setworldcoordinates(32.09969,-124.76537,42.18684,-114.00260)
+    wn.setworldcoordinates(32.09969,-124.76537,42.18684,-114.00260) #got these coordinates from google maps
     
 
     canvas = wn.getcanvas()
@@ -32,7 +33,7 @@ def CarribeanSet():
     
     wn = turtle.Screen()
     wn.title('Caribean')
-    wn.setworldcoordinates(7.24111,-94.21823,24.04272,-59.00667)
+    wn.setworldcoordinates(7.24111,-94.21823,24.04272,-59.00667) #got from google maps
     
 
     canvas = wn.getcanvas()
@@ -50,7 +51,7 @@ def SEAsiaSet(): #good
     
     wn = turtle.Screen()
     wn.title('South East Asia')
-    wn.setworldcoordinates(-10.97059,94.94849,14.96693,149.53941)
+    wn.setworldcoordinates(-10.97059,94.94849,14.96693,149.53941) #got from google maps
     
 
     canvas = wn.getcanvas()
@@ -65,16 +66,16 @@ def SEAsiaSet(): #good
 
 def distance(swaves,pwaves):
     '''Gives the amount of km from the epicenter'''
-    s_list = swaves.split(':')
+    s_list = swaves.split(':') #got this data from onlince
     p_list = pwaves.split(':')
     s_sec = int(s_list[0])*60 + int(s_list[1])
     p_sec = int(p_list[0])*60 + int(p_list[1])
-    return (p_sec-s_sec)*8.4 #might try 9.75 instead of 8.4
+    return (p_sec-s_sec)*8.4 #might try 9.75 instead of 8.4 #found 8.4 online, 9.75 from the geo class
 
 def distanceFormap(km):
     '''Gives the amount of degrees in lat from epicenter
     for the map, going south the radius to start the circle'''
-    return km/110.574
+    return km/110.574 #got from stackflow
 
 test()
 
